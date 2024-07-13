6a7dcfe82a45160744300bdf7688c048c719344c
robcimalla <rob.cimalla@gmail.com>
2024-07-13 21:44:14 +0200

    Updated Skills, Experience component

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,6 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
+import { FaCode, FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/project1.jpeg";
 import rmtdevImg from "@/public/project2.png";
@@ -35,47 +35,47 @@ export const links = [
 
 export const experiencesData = [
 	{
-		title: "Graduated bootcamp",
-		location: "Miami, FL",
-		description: "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+		title: "Graduated master degree",
+		location: "Magdeburg, DE",
+		description: "I graduated with a Master's degree in Business Economics while working as a working student in IT.",
 		icon: React.createElement(LuGraduationCap),
-		date: "2019",
+		date: "2016-2019",
 	},
 	{
-		title: "Front-End Developer",
-		location: "Orlando, FL",
-		description: "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+		title: "IT Trainee",
+		location: "Hannover, DE",
+		description: "As an IT Trainee @ E.ON, I worked within different teams, such as Public Cloud, API Management and Customer Experience.",
 		icon: React.createElement(CgWorkAlt),
 		date: "2019 - 2021",
 	},
 	{
-		title: "Full-Stack Developer",
-		location: "Houston, TX",
-		description: "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-		icon: React.createElement(FaReact),
+		title: "Software Engineer",
+		location: "Hannover, DE",
+		description: "I'm now a software engineer working on the central customer communication platform within E.ON developing custom features, tools and apps for our customers.",
+		icon: React.createElement(FaCode),
 		date: "2021 - present",
 	},
 ] as const;
 
 export const projectsData = [
 	{
-		title: "CorpComment",
-		description: "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-		tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+		title: "Customer Feedback",
+		description: "Developed Azure cloud-based, white-label landing pages for clients to collect and analyze customer feedback",
+		tags: ["Azure", "C#", "Angular", "Azure Functions", "Azure Blob Storage"],
 		imageUrl: corpcommentImg,
 	},
 	{
-		title: "rmtDev",
-		description: "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-		tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+		title: "Role-based File Management System",
+		description: "Developed a secure, role-based file management system for controlled access to server files.",
+		tags: ["Javascript", "C#", ".NET", "MySQL"],
 		imageUrl: rmtdevImg,
 	},
 	{
-		title: "Word Analytics",
-		description: "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-		tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+		title: "Personal Websites for Rentals, Portfolios, and Bookings",
+		description: "Designed personalized client websites to facilitate apartment rentals, showcase portfolios, and enable service bookings.",
+		tags: ["React", "Next.js", "TypeScript", "Tailwind", "Framer"],
 		imageUrl: wordanalyticsImg,
 	},
 ] as const;
 
-export const skillsData = ["HTML", "CSS", "JavaScript", "TypeScript", "React", "Next.js", "Node.js", "Git", "Tailwind", "Prisma", "MongoDB", "Redux", "GraphQL", "Apollo", "Express", "PostgreSQL", "Python", "Django", "Framer Motion"] as const;
+export const skillsData = ["HTML", "CSS", "JavaScript", "TypeScript", "React", "Next.js", "Node.js", "Git", "Tailwind", "Prisma", "MongoDB", "MySQL", "Express", "PostgreSQL", "C#", ".NET", "Azure", "Azure Functions", "Azure Blob Storage"] as const;
